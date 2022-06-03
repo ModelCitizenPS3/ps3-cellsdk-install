402da20161c67d40b8d63f326522916245366684
Model Citizen <gshel054@gmail.com>
2022-06-03 09:05:54 -0400

    updated ps3-cellsdk-install.sh

--- ps3-cellsdk-install.sh
+++ ps3-cellsdk-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to install IBM Cell SDk (RHEL) 3.1 in Fedora 12 (ppc) on Playstation 3.
+# Script to install IBM Cell SDk (RHEL) 3.1 in Fedora 12 (ppc64) on Playstation 3.
 # Adds a new repo config (with RPM-GPG-KEY files) to yum package manager config dirs, then has yum rebuild its cache.
 # Installs most packages from the CellSDK repos (I developed this script from the SDK install manual for a pretty complete install).
 # Script assumes yum is already configured with (functioning) standard Fedora 12 repos!!!
@@ -27,7 +27,7 @@ yum makecache
 
 yum -y install numactl.ppc numactl.ppc64 numactl-devel.ppc numactl-devel.ppc64 netpbm-devel rsync sed tcl glibc-devel.ppc64
 yum -y groupinstall "Cell Runtime Environment" "Cell Development Tools" "Cell Development Libraries" "Cell Programming Examples"
-yum -y install alf-trace cell-spu-timing cellperfctr-driver cellperfctr-tools dacs-trace fdprpro trace blas blas-devel elfspe2 lapack lapack-devel libspe2 oprofile oprofile-devel libspe2-adabinding-devel alf-compat.ppc alf-compat.ppc64 dacs-compat.ppc64 ppu-mass-compat.ppc64 libspe2-devel.ppc libspe2-devel.ppc64
+yum -y install alf-trace cell-spu-timing cellperfctr-driver cellperfctr-tools dacs-trace fdprpro trace blas blas-devel elfspe2 lapack lapack-devel libspe2 oprofile oprofile-devel libspe2-adabinding-devel libspe2-devel.ppc libspe2-devel.ppc64
 
 printf "\nREBOOT\n\n"
 
