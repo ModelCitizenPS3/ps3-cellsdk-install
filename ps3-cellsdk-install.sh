d4dcf436ea9c763b811b2e98d5f1106ef6fc119c
Model Citizen <model.citizen@ps3linux.net>
2024-02-21 01:30:31 -0500

    fixed script and repo

--- ps3-cellsdk-install.sh
+++ ps3-cellsdk-install.sh
@@ -11,15 +11,14 @@ fi
 cp -fv rpm-gpg/RPM-GPG-KEY-cellsdk /etc/pki/rpm-gpg/RPM-GPG-KEY-cellsdk
 cp -fv rpm-gpg/RPM-GPG-KEY-cellsdk-open /etc/pki/rpm-gpg/RPM-GPG-KEY-cellsdk-open
 cp -fv yum.repos.d/cellsdk.repo /etc/yum.repos.d/cellsdk.repo
-cp -fv yum.repos.d/ps3linux.repo /etc/yum.repos.d/ps3linux.repo
 
 yum clean all
 yum makecache
 
 yum -y groupinstall "Cell Development Libraries" "Cell Development Tools" "Cell Programming Examples" "Cell Runtime Environment"
-yum -y install rsync wget tcl sed ed elfspe2 libspe2.ppc libspe2.ppc64 libspe2-devel.ppc libspe2-devel.ppc64 netpbm.ppc netpbm.ppc64 netpbm-devel.ppc netpbm-devel.ppc64 alf-ide-template cell-xlc-ssc-cmp cell-xlc-ssc-help cell-xlc-ssc-lib cell-xlc-ssc-man cell-xlc-ssc-omp cell-xlc-ssc-rte cell-xlc-ssc-rte-lnk dacs-tools ppu-gcc-fortran ppu-gcc-gnat spu-gcc-fortran cell-spu-timing cellperfctr-driver cellperfctr-tools fdprpro pdt pdt-module
+yum -y install rsync wget tcl sed ed elfspe2 libspe2.ppc libspe2.ppc64 libspe2-devel.ppc libspe2-devel.ppc64 netpbm.ppc netpbm.ppc64 netpbm-devel.ppc netpbm-devel.ppc64 alf-ide-template cell-xlc-ssc-cmp cell-xlc-ssc-help cell-xlc-ssc-lib cell-xlc-ssc-man cell-xlc-ssc-omp cell-xlc-ssc-rte cell-xlc-ssc-rte-lnk dacs-tools ppu-gcc-fortran ppu-gcc-gnat spu-gcc-fortran cell-spu-timing cellperfctr-driver cellperfctr-tools fdprpro pdt
 
-printf "\nNew service (elfspe2) installed.\nREBOOT!\n\n"
+printf "\nNew service (elfspe2) installed.\nYou should reboot.\n\n"
 
 exit 0
 
