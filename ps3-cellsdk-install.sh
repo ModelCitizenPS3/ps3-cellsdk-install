b50c71f21f03d18c7e29bd8557cf092646d5b575
Model Citizen <model.citizen@ps3linux.net>
2024-06-30 12:55:07 -0400

    improved ps3-cellsdk-install.sh script

--- ps3-cellsdk-install.sh
+++ ps3-cellsdk-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
-# RUN AS ROOT - Happy SPUing!
+# Script to install the Cell Braodband Engine (Cell/B.E.) Software Development Kit (SDK)
+# To be run on a Fedora 12 (ppc) Linux system such as a Playstation 3 (with firmware <= 3.15) under OtherOS
 #
+# RUN AS ROOT
 
 if [ $(id -u) != 0 ]; then
     printf "Please run as root.\n"
@@ -14,11 +16,10 @@ cp -fv yum.repos.d/cellsdk.repo /etc/yum.repos.d/cellsdk.repo
 
 yum clean all
 yum makecache
-
 yum -y groupinstall "Cell Development Libraries" "Cell Development Tools" "Cell Programming Examples" "Cell Runtime Environment"
 yum -y install rsync wget tcl sed ed gdb-gdbserver elfspe2 libspe2.ppc libspe2.ppc64 libspe2-devel.ppc libspe2-devel.ppc64 netpbm.ppc netpbm.ppc64 netpbm-devel.ppc netpbm-devel.ppc64 alf-ide-template cell-xlc-ssc-cmp cell-xlc-ssc-help cell-xlc-ssc-lib cell-xlc-ssc-man cell-xlc-ssc-omp cell-xlc-ssc-rte cell-xlc-ssc-rte-lnk dacs-tools ppu-gcc-fortran ppu-gcc-gnat spu-gcc-fortran cell-spu-timing cellperfctr-driver cellperfctr-tools fdprpro pdt cell-libs.ppc cell-libs.ppc64 cell-libs-devel.ppc cell-libs-devel.ppc64 numactl.ppc numactl.ppc64 numactl-devel.ppc numactl-devel.ppc64
 
-printf "\nNew service (elfspe2) installed.\nYou should reboot.\n\n"
+printf "\nCell SDK install complete! Thank your for running my script :-)\n\nNOTE:\nYou should add 'export CELL_TOP=/opt/cell/sdk' to your .bashrc file.\nAlso, new service (elfspe2) has been installed. Reboot to enable.\n\n"
 
 exit 0
 
